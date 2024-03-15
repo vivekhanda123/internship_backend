256f08b8d62121c5283ece97164a496d59dc3219
vivekhanda123 <vivekhandaindore@gmail.com>
2024-03-15 17:30:21 +0530

    updated vercel file

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ require("../src/db/conn");
 require("dotenv").config();
 const port = process.env.PORT || 8000;
 // const socketIO = require("socket.io");
-const router = require("../src/routers/main");
+const router = require("../src/routes/main");
 const cookieParser = require("cookie-parser"); //extract info/data from cookie
 const bodyParser = require("body-parser") // parsing incoming request in bodies
 const cors = require("cors"); //front-end client can make requests for resources from backend 
