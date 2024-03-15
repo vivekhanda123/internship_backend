ebc0d98dde0d888615832e2df80d1af5bffa98cd
vivekhanda123 <vivekhandaindore@gmail.com>
2024-03-15 15:52:22 +0530

    hide the db keys

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 // mongoose.connect("mongodb://0.0.0.0:27017/demo")
-mongoose.connect("mongodb+srv://vivekhandaindore:961QR5cXzQFfr9FT@cluster0.j0hkqu2.mongodb.net/")
+mongoose.connect(process.env.DB)
 .then(()=>{
     console.log("Connection Successful");
 }).catch((e)=>{
