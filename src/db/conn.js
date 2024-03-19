d6ecc1f958f970b22140b726a98d2a2377e4be08
vivekhanda123 <vivekhandaindore@gmail.com>
2024-03-19 15:50:41 +0530

    added

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
-mongoose.connect("mongodb://0.0.0.0:27017/demo")
-// mongoose.connect(process.env.DB_LINK)
+// mongoose.connect("mongodb://0.0.0.0:27017/demo")
+mongoose.connect(process.env.DB_LINK)
 .then(()=>{
     console.log("Connection Successful");
 }).catch((e)=>{
